dbbe837bb854fb876f4f92d2f2f94c3567e2e08c
Vegard Endresen <veen@nordicsemi.no>
2017-09-07 16:43:01 +0200

    Adding plugin install script

---
+++ config/vim/.vim/bundle/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Clone plugins
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/scrooloose/nerdtree.git nerdtree
+git clone https://github.com/vim-scripts/tcd.vim.git
