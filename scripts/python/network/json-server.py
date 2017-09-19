c69c532c1e3c520c1df6dd81b35de84197cd4ea8
Vegard Endresen <veen@nordicsemi.no>
2017-09-19 11:00:59 +0200

    JSON server fixes

--- scripts/python/network/json-server.py
+++ scripts/python/network/json-server.py
@@ -11,8 +11,8 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
 
   def do_GET(self):
     self._set_headers()
-    msg = "<html><body><h2>Got GET!</h2></body></html>"
-    self.wfile.write(mgs.encode("utf-8"))
+    msg = "<html><body><h3>JSON server, GET request.</h3></body></html>" self.wfile.write(msg.encode("utf-8"))
+    print("%s:GET"  %(self.client_address[0]))
 
   def do_HEAD(self):
     self._set_headers()
@@ -20,13 +20,18 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
   def do_POST(self):
     # Doesn't do anything with posted data
     self._set_headers()
-    msg = "<html><body><h2>Got POST!</h2></body></html>"
+    msg = "<html><body><h3>JSON server, POST request</h3></body></html>"
     self.wfile.write(msg.encode("utf-8"))
     tmp = self.rfile.read(int(self.headers['Content-Length']))
-    print(tmp.decode("utf-8"))
+    print("%s:%s" %(self.client_address[0],tmp.decode("utf-8")))
+
+  # Make quiet (XXX: Remove when debugging)
+  #https://stackoverflow.com/questions/10651052/how-to-quiet-simplehttpserver
+  def log_message(self, format, *args):
+    a=1
 
 PORT = 9090
-
+socketserver.TCPServer.allow_reuse_address = True
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
   print("serving at port", PORT)
   httpd.serve_forever()
