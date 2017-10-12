53245da3c239d2fb28bc7d66578aaf2f0655c696
Vegard Endresen <veen@nordicsemi.no>
2017-10-12 21:53:33 +0200

    Extended FAQ for i3 sizing

--- config/i3/install.sh
+++ config/i3/install.sh
@@ -1,3 +1,8 @@
 -- Fix font sizes
+1) lx-appearance
 https://askubuntu.com/questions/598943/how-to-de-uglify-i3-wm
 sudo apt-get install lxappearance gtk-chtheme qt4-qtconfig
+
+2) Edit ~/.config/i3/config, change size for
+font pango:monospace 14
+
