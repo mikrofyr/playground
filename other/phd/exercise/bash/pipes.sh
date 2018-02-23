a5dfe3840d32b5e9d2918aa3fb2fda465e9a124a
Vegard Endresen <veen@nordicsemi.no>
2018-02-23 09:02:34 +0100

    Adding bash exercise

---
+++ other/phd/exercise/bash/pipes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# 1) Print the last two lines of data.csv (tail)
+
+# 2) Print the first two lines of data.csv (head)
+
+# 3) Print the first two lines of data.csv, excluding the header (head, grep)
+
+# 4) Replace each comma with single whitespace (sed)
+
+# 5) Print second column (sed as above + awk)
+
+# 6) Print description for all cars made in 1999
+
