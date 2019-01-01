a0ecd2d8fb4834570f128d2e39a218bc9ac96f3d
Vegard Endresen <veen@nordicsemi.no>
2019-01-01 21:29:50 +0100

    pi-led updates

--- projects/pi-led/button_machine.py
+++ projects/pi-led/button_machine.py
@@ -28,6 +28,14 @@ buttons = {}
 buttons[BUTTON] = 26
 
 # ------------------------------
+# -- Callback
+# ------------------------------
+mode = 0
+def button_callback(arg):
+  global mode
+  mode += 1
+
+# ------------------------------
 # -- Configure GPIO
 # ------------------------------
 GPIO.setwarnings(False)
@@ -35,31 +43,49 @@ GPIO.setmode(GPIO.BCM)
 for key in leds:
   GPIO.setup(leds[key],GPIO.OUT)
 
-#for key in buttons:
-#  GPIO.setup(buttons[key],GPIO.IN)
-
-
-#GPIO.setup(buttons[BUTTON], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#GPIO.add_event_detect(buttons[BUTTON],GPIO.RISING,callback=button_callback)
-# def button_callback:
-#
+for key in buttons:
+  GPIO.setup(buttons[key], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  GPIO.add_event_detect(buttons[BUTTON], GPIO.RISING, callback=button_callback, bouncetime=300)
 
 # ------------------------------
-# -- Program
+# -- Main loop
 # ------------------------------
+def set_led(a):
+  global leds
+  bitmask = {}
+  bitmask[GREEN] = 0b1
+  bitmask[YELLOW] = 0b10
+  bitmask[RED] = 0b100
 
-#for key in leds:
-#  print(GPIO.input(leds[key]))
-
-bitmask = {}
-bitmask[GREEN] = 0b1
-bitmask[YELLOW] = 0b10
-bitmask[RED] = 0b100
-
-for i in range(0,4):
   for key in leds:
-    value = False
-    if i & bitmask[key]:
-      value = True
-    GPIO.output(leds[key], value)
-  time.sleep(1)
+    state = False
+    if a & bitmask[key]:
+      state = True
+    GPIO.output(leds[key], state)
+
+def invert_led():
+  global leds
+  for key in leds:
+    GPIO.output(leds[key], not GPIO.input(leds[key]))
+
+def led_off():
+  for key in leds:
+    GPIO.output(leds[key], False)
+
+cnt = 0
+
+while True:
+  if mode == 0:
+    set_led(cnt)
+    cnt += 1 
+    time.sleep(1)
+  elif mode ==1:
+    invert_led()
+    time.sleep(1)
+  elif mode ==2:
+    led_off()
+    break
+
+#message = input("Press enter to quit\n\n")
+GPIO.cleanup()
+
