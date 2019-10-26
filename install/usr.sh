f6609ff72b2ae882cc30958965b76a48224b7179
Vegard Endresen <veen@nordicsemi.no>
2019-10-26 20:31:23 +0200

    script fixup and share location

--- install/usr.sh
+++ install/usr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "usr.sh"
+
 if [ -d ~/usr/bin ]; then
   echo "Error: ~/usr/bin already exists"
   exit 1
@@ -14,16 +16,17 @@ if [ -d ~/usr/share ]; then
   exit 1
 fi
 
-BIN_PATH="../../usr/bin"
-LIB_PATH="../../usr/lib"
-SHARE_PATH="../../usr/share"
+BIN_PATH="../usr/bin"
+LIB_PATH="../usr/lib"
+SHARE_PATH="../usr/share"
+pwd
 
 if [ ! -d $BIN_PATH ] ||  [ ! -d $LIB_PATH ] || [ ! -d $SHARE_PATH ]; then
   echo "Error: Could not find source folders"
   exit 1
 fi
 
-ln -s $BIN_PATH ~/usr/bin
-ln -s $LIB_PATH ~/usr/lib
-ln -s $SHARE_PATH ~/usr/share
+ln -s $PWD/$BIN_PATH ~/usr/bin
+ln -s $PWD/$LIB_PATH ~/usr/lib
+ln -s $PWD/$SHARE_PATH ~/usr/share
 
