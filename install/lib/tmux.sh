0fd6026c64f10298b225d9b67e69b396a122230a
asd <asd@asd.no>
2020-01-29 20:10:21 +0100

    Tmux install

---
+++ install/lib/tmux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "usr.sh"
+
+TMUX_PATH="../usr/share/tmux"
+pwd
+
+if [ ! -d $TMUX_PATH ] ; then
+  echo "Error: Could not find source folder"
+  exit 1
+fi
+
+ln -s $PWD/$TMUX_PATH/.tmux.conf ~/.tmux.conf
+ln -s $PWD/$TMUX_PATH/.tmux.conf.local ~/.tmux.conf.local
