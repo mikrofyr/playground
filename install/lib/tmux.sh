613ffdce27e34bf9e26e97ebfb4f5bf1b7f36e9e
veen <veen@nordicsemi.no>
2020-08-31 12:50:46 +0200

    tmuxinator config install

--- install/lib/tmux.sh
+++ install/lib/tmux.sh
@@ -13,3 +13,11 @@ fi
 ln -s $PWD/$TMUX_PATH/.tmux.conf ~/.tmux.conf
 ln -s $PWD/$TMUX_PATH/.tmux.conf.local ~/.tmux.conf.local
 cp $PWD/$TMUX_PATH/.tmux.conf.shell ~/.tmux.conf.shell
+
+
+TMUXINATOR_PATH="../usr/share/tmuxinator"
+if [ -d $TMUXINATOR_PATH ] ; then
+  mkdir -p ~/.config/tmuxinator/
+  cp $PWD/$TMUXINATOR_PATH/main.yml ~/.config/tmuxinator
+fi
+
