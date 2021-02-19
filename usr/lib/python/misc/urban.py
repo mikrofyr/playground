624c35a049dbac8a4e4f1134774978a4b1b38ce5
Vegard Endresen <vegarend@gmail.com>
2021-02-19 14:13:48 +0100

    urban dictionary script

---
+++ usr/lib/python/misc/urban.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import urllib.request
+import json      
+import argparse
+
+parser = argparse.ArgumentParser(description='Argparse example')
+parser.add_argument('-q','--query', help='Word to search for', required=True)
+args = parser.parse_args()
+
+myurl = "http://api.urbandictionary.com/v0/define?term={}".format(args.query)
+req = urllib.request.Request(myurl)
+#req.add_header('Content-Type', 'application/json; charset=utf-8')
+#jsondata = json.dumps(body)
+#jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+#req.add_header('Content-Length', len(jsondataasbytes))
+#print (jsondataasbytes)
+response = urllib.request.urlopen(req)
+jd = response.read()
+#print(jd)
+data =json.loads(jd.decode('utf-8'))
+#print(data)
+max = 0
+word = ""
+#for item in data['list']:
+#  if item['thumbs_up'] > max:
+#    max = item['thumbs_up']
+#    word = item['definition']
+    
+print(data['list'][0]['definition'])
+print(data['list'][0]['example'])
